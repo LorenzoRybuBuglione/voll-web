8939940d8425beabfe6a33360d57b78761ea6a0e
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-10-18 18:07:34 -0300

    Dynamic Graphs

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import "./App.css";
 import Cabecalho from "./components/Cabecalho";
 import Container from "./components/Container";
+import Grafico from "./components/Grafico";
 import Rodape from "./components/Rodape";
 import Tabela from "./components/Tabela";
 import Titulo from "./components/Titulo";
 import useDadosConsulta from "./useDadosConsulta";
+import useDadosProfissional from "./useDadosProfissionais";
 
 function App() {
-    const { dados } = useDadosConsulta();
+    const { dados: consultas, erro: consultasErro } = useDadosConsulta();
+    const { dados: profissionais, erro: profissionaisErro } = useDadosProfissional();
+
+    if(consultasErro ||  profissionaisErro) {
+        console.log("Ocorreu um erro na reguisição")
+    }
+
 
     return (
         <>
             <Cabecalho />
             <Container>
                 <Titulo>Área Administrativa</Titulo>
-                <Tabela consultas={dados} />
+                <Tabela consultas={consultas} />
+                <Grafico consultas={consultas} profissionais={profissionais}/>
             </Container>
             <Rodape />
         </>
