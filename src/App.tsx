8b7ebf8193d2f82136349abc7edcf6861eecdc68
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-10-18 17:36:10 -0300

    Dynamic Table

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import "./App.css";
 import Cabecalho from "./components/Cabecalho";
 import Container from "./components/Container";
 import Rodape from "./components/Rodape";
+import Tabela from "./components/Tabela";
 import Titulo from "./components/Titulo";
+import useDadosConsulta from "./useDadosConsulta";
 
 function App() {
+    const { dados } = useDadosConsulta();
+
     return (
         <>
             <Cabecalho />
             <Container>
                 <Titulo>Área Administrativa</Titulo>
+                <Tabela consultas={dados} />
             </Container>
             <Rodape />
         </>
