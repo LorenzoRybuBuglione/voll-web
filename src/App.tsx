331cf6d30398f16065669d6588f05287a5e1e729
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-10-19 16:04:59 -0300

    Cards, subtitle and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Avaliacao from "./components/Avaliacao";
+import Botao from "./components/Botao";
 import Cabecalho from "./components/Cabecalho";
 import Container from "./components/Container";
 import Grafico from "./components/Grafico";
 import Rodape from "./components/Rodape";
+import Subtitulo from "./components/Subtitulo";
 import Tabela from "./components/Tabela";
 import Titulo from "./components/Titulo";
 import useDadosConsulta from "./useDadosConsulta";
@@ -22,8 +25,16 @@ function App() {
             <Cabecalho />
             <Container>
                 <Titulo>Área Administrativa</Titulo>
+                <Botao>Cadastrar especialista</Botao>
+                <Titulo imagem="consulta">Consultas do Dia</Titulo>
                 <Tabela consultas={consultas} />
+                <Botao>Ver mais</Botao>
+                <Titulo imagem="grafico">Consultas mensais por especialista</Titulo>
                 <Grafico consultas={consultas} profissionais={profissionais}/>
+                <Titulo imagem="avaliacao">Avaliação de especialistas</Titulo>
+                <Subtitulo>Dezembro/22</Subtitulo>
+                <Avaliacao profissionais={profissionais}/>
+                <Botao>Ver mais</Botao>
             </Container>
             <Rodape />
         </>
---
+++ src/components/Avaliacao/Card/index.tsx
@@ -0,0 +1,80 @@
+import { Rating } from "@mui/material";
+import styled from "styled-components";
+import IProfissional from "../../../types/IProfissional";
+
+const ContainerEstilizado = styled.div`
+    flex: 0.4;
+    max-width: 100%;
+    background-color: #ffffff;
+    padding: 1em;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.15);
+    border-radius: 8px;
+    color: var(--cinza);
+    margin: 1em 2em 1em;
+`;
+
+const ListaEstilizada = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0;
+`;
+
+const ItemEstilizado = styled.li`
+    list-style-type: none;
+`;
+
+const ItemInformacoesEstilizado = styled(ItemEstilizado)`
+    flex-grow: 1;
+`;
+
+const ImagemEstilizada = styled.img`
+    width: 64px;
+    height: 64px;
+    border: 2px solid var(--azul-claro);
+    border-radius: 100%;
+    margin-right: 1em;
+    object-fit: cover;
+`;
+
+const ParagrafoNomeEstilizado = styled.p`
+    font-weight: 700;
+`;
+
+const ParagrafoEstilizado = styled.p`
+    margin: .5em 0;
+    font-size: 14px;
+`;
+
+function Card({ profissional }: { profissional: IProfissional }) {
+    return (
+        <ContainerEstilizado>
+            <ListaEstilizada>
+                <ItemEstilizado>
+                    <ImagemEstilizada
+                        src={profissional.imagem}
+                        alt={`Foto de perfil do profissional ${profissional.nome}`}
+                    />
+                </ItemEstilizado>
+                <ItemInformacoesEstilizado>
+                    <ParagrafoNomeEstilizado>
+                        {profissional.nome}
+                    </ParagrafoNomeEstilizado>
+                    <ParagrafoEstilizado>
+                        {profissional.especialidade}
+                    </ParagrafoEstilizado>
+                </ItemInformacoesEstilizado>
+                <ItemEstilizado>
+                    <Rating
+                        name="simple-controlled"
+                        value={profissional.nota}
+                        readOnly
+                    />
+                </ItemEstilizado>
+            </ListaEstilizada>
+        </ContainerEstilizado>
+    );
+}
+
+export default Card;
